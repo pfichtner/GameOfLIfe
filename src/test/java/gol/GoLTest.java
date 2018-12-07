c2e0f6ad81603a9f2cf56e366e4084d0a6fa0fba
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-12-07 23:09:26 +0100

    autocommit on change

--- src/test/java/gol/GoLTest.java
+++ src/test/java/gol/GoLTest.java
@@ -61,6 +61,7 @@ public class GoLTest {
 	}
 
 	private TypeSafeMatcher<Board> board(String... rows) {
+		String expected = range(0, board.getHeight()).mapToObj(y -> row(y)).collect(joining("\n"));
 		return new TypeSafeMatcher<Board>() {
 
 			@Override
@@ -71,7 +72,7 @@ public class GoLTest {
 
 			@Override
 			public boolean matchesSafely(Board board) {
-				String expected = range(0, board.getHeight()).mapToObj(y -> row(y)).collect(joining("\n"));
+				String actual = range(0, board.getHeight()).mapToObj(y -> row(y)).collect(joining("\n"));
 				// TODO Auto-generated method stub
 				return false;
 			}
