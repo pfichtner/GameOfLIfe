a747b64da25b726d7732a106726ff3ed13b35106
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-12-07 22:41:15 +0100

    autocommit on change

--- src/test/java/gol/GoLTest.java
+++ src/test/java/gol/GoLTest.java
@@ -82,8 +82,6 @@ public class GoLTest {
 	}
 
 	private void whenTicked() {
-		Set<Point> nextGen = new HashSet<Point>();
-
 		IntStream yr = IntStream.range(0, height);
 		Stream<Stream<Point>> mapToObj = yr.mapToObj(y -> IntStream.range(0, height).mapToObj(x -> new Point(x, y)));
 		Stream<Point> points = mapToObj.flatMap(identity());
