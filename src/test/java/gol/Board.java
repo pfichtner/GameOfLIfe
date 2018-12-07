582b0601839c7d003cca95a91f7941a710448b27
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-12-07 22:51:09 +0100

    autocommit on change

--- src/test/java/gol/Board.java
+++ src/test/java/gol/Board.java
@@ -61,7 +61,7 @@ public class Board {
 
 	public void tick() {
 		setLifeCells(IntStream.range(0, getHeight())
-				.mapToObj(y -> IntStream.range(0, board.getHeight()).mapToObj(x -> new Point(x, y))).flatMap(identity())
+				.mapToObj(y -> IntStream.range(0, getHeight()).mapToObj(x -> new Point(x, y))).flatMap(identity())
 				.filter(this::alifeInNextGen).collect(toSet()));
 
 	}
