9ad08b3e91189a7a47ffd4393ff1539648e8daa5
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-12-07 22:11:27 +0100

    autocommit on change

--- src/test/java/gol/GoLTest.java
+++ src/test/java/gol/GoLTest.java
@@ -1,5 +1,38 @@
 package gol;
 
+import static org.junit.Assert.*;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
 public class GoLTest {
 
+	@Test
+	public void canCreateBoardAndSetLife() {
+		aNewBoard(1,1);
+		withLifeAt(0,0);
+		hasLifeAt(0,0);
+		
+	}
+
+	private void hasLifeAt(int i, int j) {
+		assertThat(isLifeAt(i,j), CoreMatchers.is(true));
+		
+	}
+
+	private boolean isLifeAt(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void withLifeAt(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void aNewBoard(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
