6afb9b00bc1fdb002b3cf42b03f9f92e39ebee2b
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-12-08 00:00:17 +0100

    autocommit on change

--- src/test/java/gol/Board.java
+++ src/test/java/gol/Board.java
@@ -62,9 +62,8 @@ public class Board {
 	private boolean alifeInNextGen(Point point) {
 		boolean alife = isLifeAt(point);
 		long alifeNeighbours = alifeNeighbours(point);
-		boolean b1 = alife && (alifeNeighbours == 2 || alifeNeighbours == 3);
 		boolean b2 = !alife && (alifeNeighbours == 3);
-		return b1 || b2;
+		return alife && (alifeNeighbours == 2 || alifeNeighbours == 3) || b2;
 	}
 
 	private long alifeNeighbours(Point thisPoint) {
