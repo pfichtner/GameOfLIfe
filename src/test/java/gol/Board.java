4ccf7435a5f6e9ce58f8ac15dcc1345e75db4755
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-12-07 22:50:13 +0100

    autocommit on change

--- src/test/java/gol/Board.java
+++ src/test/java/gol/Board.java
@@ -7,6 +7,28 @@ import gol.GoLTest.Point;
 
 public class Board {
 
+	private static class Point {
+
+		private int x, y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int hashCode() {
+			return 31 * x + y;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			Point other = (Point) obj;
+			return other.x == x && other.y == y;
+		}
+
+	}
+
 	private Set<Point> lifeCells = new HashSet<Point>();
 	private int width;
 	private int height;
