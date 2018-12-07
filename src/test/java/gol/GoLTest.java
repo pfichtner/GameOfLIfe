7639545d8a9f6c55fac3b4582a913eb7d5f6f6b2
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-12-07 22:11:37 +0100

    autocommit on change

--- src/test/java/gol/GoLTest.java
+++ src/test/java/gol/GoLTest.java
@@ -9,15 +9,15 @@ public class GoLTest {
 
 	@Test
 	public void canCreateBoardAndSetLife() {
-		aNewBoard(1,1);
-		withLifeAt(0,0);
-		hasLifeAt(0,0);
-		
+		aNewBoard(1, 1);
+		withLifeAt(0, 0);
+		hasLifeAt(0, 0);
+
 	}
 
 	private void hasLifeAt(int i, int j) {
-		assertThat(isLifeAt(i,j), CoreMatchers.is(true));
-		
+		assertThat(isLifeAt(i, j), CoreMatchers.is(true));
+
 	}
 
 	private boolean isLifeAt(int i, int j) {
@@ -26,12 +26,12 @@ public class GoLTest {
 
 	private void withLifeAt(int i, int j) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void aNewBoard(int i, int j) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
