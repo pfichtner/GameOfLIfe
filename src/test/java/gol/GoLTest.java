c2db62fa1ce0bf69d8a3f86c43551541fe6d0cba
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-12-07 22:42:33 +0100

    autocommit on change

--- src/test/java/gol/GoLTest.java
+++ src/test/java/gol/GoLTest.java
@@ -82,8 +82,7 @@ public class GoLTest {
 	}
 
 	private void whenTicked() {
-		Stream<Stream<Point>> mapToObj = IntStream.range(0, height).mapToObj(y -> IntStream.range(0, height).mapToObj(x -> new Point(x, y)));
-		Stream<Point> points = mapToObj.flatMap(identity());
+		Stream<Point> points = IntStream.range(0, height).mapToObj(y -> IntStream.range(0, height).mapToObj(x -> new Point(x, y))).flatMap(identity());
 		Stream<Point> filter = points.filter(this::alifeInNextGen);
 		this.lifeCells = filter.collect(Collectors.toSet());
 	}
