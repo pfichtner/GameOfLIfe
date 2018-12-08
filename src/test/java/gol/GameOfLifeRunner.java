0f9d23980494e8122ff9e60ffcec9b11b227c8b6
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-12-08 15:27:12 +0100

    cleanup

--- src/test/java/gol/GameOfLifeRunner.java
+++ src/test/java/gol/GameOfLifeRunner.java
@@ -11,12 +11,16 @@ public class GameOfLifeRunner {
 	public static void main(String[] args) throws InterruptedException {
 		Board board = init(new Board(120, 40));
 		while (true) {
-			System.out.print("\033[H\033[2J");
-			range(0, board.getHeight()).forEach(y -> System.out.println(
-					range(0, board.getWidth()).mapToObj(x -> board.isAlive(x, y) ? "*" : " ").collect(joining())));
+			dumpBoard(board);
 			TimeUnit.MILLISECONDS.sleep(100);
 			board.tick();
 		}
+	}
+
+	private static void dumpBoard(Board board) {
+		System.out.print("\033[H\033[2J");
+		range(0, board.getHeight()).forEach(y -> System.out
+				.println(range(0, board.getWidth()).mapToObj(x -> board.isAlive(x, y) ? "*" : " ").collect(joining())));
 	}
 
 	private static Board init(Board board) {
