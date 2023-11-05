e2bbba14f8af1ea9c4d38cf9f7c07239d102a115
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-11-05 17:45:56 +0100

    house keeping

--- src/test/java/gol/GolStringTemplate.java
+++ src/test/java/gol/GolStringTemplate.java
@@ -3,7 +3,6 @@ package gol;
 import static java.util.stream.IntStream.range;
 
 import java.util.function.Function;
-import java.util.stream.IntStream;
 
 public class GolStringTemplate {
 
@@ -19,15 +18,14 @@ public class GolStringTemplate {
 	}
 
 	public static final StringTemplate.Processor<Board, RuntimeException> GOL = StringTemplate.Processor //
-			.of((StringTemplate template) -> stringToBoard(template.interpolate()));
-	
+			.of(t -> stringToBoard(t.interpolate()));
+
 	private static Board stringToBoard(String textblock) {
 		String[] rows = textblock.split("\n");
 		Board board = new Board(rows[0].length(), rows.length);
 		range(0, rows.length).forEach(y -> {
 			char[] charArray = rows[y].toCharArray();
-			IntStream.range(0, charArray.length).filter(x -> charArray[x] == 'X')
-					.forEach(x -> board.setAlive(x, y));
+			range(0, charArray.length).filter(x -> charArray[x] == 'X').forEach(x -> board.setAlive(x, y));
 		});
 		return board;
 
